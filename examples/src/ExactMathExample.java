47be1326794e05f14f2ec8a5e7fb447333af48a5
Daniel Cheng <daniel@danielcheng.ca>
2017-02-22 00:56:12 -0500

    Exact math, streams base

--- examples/src/ExactMathExample.java
+++ examples/src/ExactMathExample.java
@@ -1,7 +1,6 @@
 public class ExactMathExample {
     public static void main(String[] args) {
         int oneHundredMillion = 100_000_000;
-        System.out.println(oneHundredMillion * oneHundredMillion);
         System.out.println(Math.multiplyExact(oneHundredMillion, oneHundredMillion));
     }
 }
